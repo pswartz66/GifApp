8fa3e276b884cade42e302625875f2661fda4bc6
Phil Swartz <pswartz.sju@gmail.com>
2019-09-13 22:05:55 -0400

    added main startup files and basic giphy API call

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+var API_Key = "4JhZrt5joIBhUY2fA3kMRtug4WWhJ13G";
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key="+API_Key+"&q=Michael+Phelps&limit=2&offset=0&rating=G&lang=en"
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+
+    console.log(response);
+    console.log(response.data[0].images.downsized);
+});
